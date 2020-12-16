2da13bf8ca916749daa4ef890d1f17229d81aa57
kostenk0 <tnellsu@gmail.com>
2020-12-16 22:33:14 +0200

    signup

--- Web/src/pages/Profile.jsx
+++ Web/src/pages/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import AuthenticatedPage from './AuthenticatedPage.jsx';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import UserRating from '../components/UserRating.jsx';
 import Carousel from '../components/Carousel.jsx';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import _ from 'lodash';
 import * as ProfileActions from '../redux/actions/ProfileActions';
 
@@ -15,8 +15,8 @@ class Profile extends React.Component {
   }
 
   render() {
-    var {profile, ratedMovies, recommendedMovies} = this.props.profile;
-    var {profileRateMovie, profileDeleteMovieRating} = this.props;
+    var { profile, ratedMovies, recommendedMovies } = this.props.profile;
+    var { profileRateMovie, profileDeleteMovieRating } = this.props;
 
     if (!profile) {
       return null;
@@ -24,28 +24,9 @@ class Profile extends React.Component {
 
     return (
       <div className="nt-profile">
-        {/*isFetching ? <Loading/> : null*/}
         <div className="row">
-          <div className="small-12 columns">
-            <div className="nt-box">
-              <div className="nt-box-title">
-                My Profile
-              </div>
-              <div className="nt-box-row">
-                <div className="row">
-                  <div className="small-12 medium-2 large-2 columns">
-                    <div className="nt-profile-gravatar">
-                      <img src={_.get(profile, 'avatar.fullSize')} alt="" />
-                    </div>
-                  </div>
-                  <div className="small-12 medium-10 large-10 columns">
-                    <div className="nt-profile-first-name">
-                      User Name: {profile.username}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="large-12 columns">
+            <h2 className="nt-movie-title">My Profile: {profile.username}</h2>
           </div>
         </div>
 
@@ -61,7 +42,7 @@ class Profile extends React.Component {
                     return (
                       <div key={movie.id}>
                         <Link to={`/movie/${movie.id}`}>
-                          <img src={movie.posterImage} className="nt-profile-movie-cover" alt=""/>
+                          <img src={movie.posterImage} className="nt-profile-movie-cover" alt="" />
                         </Link>
                         <div className="nt-profile-movie-title">
                           <Link to={`/movie/${movie.id}`}>
@@ -70,9 +51,9 @@ class Profile extends React.Component {
                         </div>
                         <div>
                           <UserRating movieId={movie.id}
-                                      savedRating={movie.myRating}
-                                      onSubmitRating={profileRateMovie}
-                                      onDeleteRating={profileDeleteMovieRating}/>
+                            savedRating={movie.myRating}
+                            onSubmitRating={profileRateMovie}
+                            onDeleteRating={profileDeleteMovieRating} />
                         </div>
                       </div>
                     );
--- Web/src/pages/Signup.jsx
+++ Web/src/pages/Signup.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import validatedComponent from '../components/validation/ValidatedComponent.jsx';
-import InputValidator from '../components/validation/InputValidator.jsx';
 import * as Actions from '../redux/actions/ProfileActions';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Form, Col, Button, Row } from 'react-bootstrap';
 
 class Signup extends React.Component {
   constructor() {
@@ -29,7 +29,7 @@ class Signup extends React.Component {
   }
 
   redirectIfAuthed(props) {
-    var {match, history, auth} = props;
+    var { match, history, auth } = props;
     if (auth.token) {
       if (match.params.redirectTo) {
         history.push(match.params.redirectTo);
@@ -42,79 +42,66 @@ class Signup extends React.Component {
 
   createUser(event) {
     event.preventDefault();
-    var {username, password} = this.state;
+    var { username, password } = this.state;
 
     if (this.props.isComponentValid()) {
-      this.props.createProfile({username, password});
+      this.props.createProfile({ username, password });
     }
   }
 
   onChange(fieldName, e) {
-    this.setState({[fieldName]: e.target.value});
+    this.setState({ [fieldName]: e.target.value });
   }
 
   validateConfirmPassword() {
-    var {password, confirmPassword} = this.state;
+    var { password, confirmPassword } = this.state;
     if (password !== confirmPassword) {
       return 'Both passwords must be equal';
     }
   }
 
   render() {
-    var {state} = this;
-    var {errors} = this.props;
+    var { state } = this;
+    var { errors } = this.props;
     return (
-      <div className="ba-signup row">
-        <form noValidate>
-          <div className="panel small-12 small-centered columns">
-            <div className="row panel-title">
-              <h3>Create an Account</h3>
-            </div>
-            <div className="row">
-              <InputValidator fieldName="User name"
-                              errors={errors.username}
-                              shouldValidateOnBlur={true}>
-              <input type="text"
-                     name="name"
-                     required
-                     placeholder="User name*"
-                     value={state.username}
-                     onChange={this.onChange.bind(this, 'username')}/>
-              </InputValidator>
-            </div>
-            <div className="row">
-              <input type="password"
-                     name="password"
-                     placeholder="Password*"
-                     required
-                     onChange={this.onChange.bind(this, 'password')}
-                     value={state.password}/>
-            </div>
-            <div className="row">
-              <InputValidator fieldName="Password"
-                              errors={errors.password}
-                              shouldValidateOnBlur={true}
-                              customValidation={() => {return this.validateConfirmPassword();}}>
-              <input type="password"
-                     name="password-confirm"
-                     placeholder="Confirm Password*"
-                     required
-                     onChange={this.onChange.bind(this, 'confirmPassword')}
-                     value={state.confirmPassword}/>
-              </InputValidator>
-            </div>
-            <div className="row text-center">
-              <button type="button"
-                      name="btn-create"
-                      className="ba-default-button"
-                      onClick={this.createUser}>
-                Create Account
-              </button>
-            </div>
-          </div>
-        </form>
-        <div className="push"/>
-      </div>
+      <Form>
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+
+          <Form.Label column sm={2}>
+            User name
+    </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="User name" errors={errors.username} shouldValidateOnBlur={true}
+              value={state.username} onChange={this.onChange.bind(this, 'username')} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Password
+    </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" onChange={this.onChange.bind(this, 'password')}
+              value={state.password} errors={errors.password} onChange={this.onChange.bind(this, 'password')} value={state.password} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Confirm Password
+    </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Confirm Password" errors={errors.password} shouldValidateOnBlur={true}
+              customValidation={() => { return this.validateConfirmPassword(); }} onChange={this.onChange.bind(this, 'confirmPassword')} value={state.confirmPassword} />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 5 }}>
+            <Button type="submit" onClick={this.createUser}>Sign in</Button>
+          </Col>
+        </Form.Group>
+      </Form>
     );
   }
 }
@@ -126,7 +113,7 @@ function mapDispatchToProps(dispatch) {
 }
 
 function mapStateToProps(state) {
-  return {...state.signup,  auth: {...state.auth}};
+  return { ...state.signup, auth: { ...state.auth } };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(validatedComponent(withRouter(Signup)));
